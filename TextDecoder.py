18bbd6ece2e54906a208cef8137505d4d8485103
ddmedina <64035608+ddmedina@users.noreply.github.com>
2020-05-07 15:27:27 -0700

    Add files via upload

---
+++ TextDecoder.py
@@ -0,0 +1,104 @@
+keyNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+keyLowerAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+keyHigherAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+keySPChar = ['!', '@', '#', '$', '%', '&', ' ', '_', '-', '+', '/','?','*', '.', ',' , ':', ';', "'", '!', '@', '#', '$', '%', '&', ' ', '_', '-', '+', '/','?','*', '.', ',' , ':', ';', "'"]
+             
+class Error(Exception):
+    pass
+class NoEncryption(Error):
+    pass
+
+while(True):
+    fileName = input("Enter the name of the encrypted file you wish to open: ")
+    try:
+        f = open(fileName + ".txt", 'r')
+        line1 = f.readline(-1).strip()
+        if line1 != "3NCRY":
+            raise NoEncryption
+    except NoEncryption:
+        print("This file is not encrypted! Please try again.")
+    else:
+        break
+
+# Reads in the offset number along with the message
+encryptedArray = f.readline(-1).split('^')
+offset = encryptedArray[0]
+encodedMessage = encryptedArray[1].rstrip("\n")
+
+# Reads the rotated character type array of the encoded file
+typeArray = f.readline(-1).split("^")
+typeArray.pop()             #Removes the n\ character
+
+
+# Reads in the index array for each character
+IndexArray = f.readline(-1).split(".")
+IndexArray.pop()            #Removes the \n character
+IndexArray.reverse()
+
+
+# Reads in the key for the rotated character type array
+# and converts each entyr to an int, which is then appended 
+# to a new array
+rotatorKeyArray = f.readline(-1).split(".")
+rotatorKeyArray.pop()       #Removes the \n character
+rKeyArray = []
+for i in range(len(rotatorKeyArray)):
+    rKeyArray.append(int(rotatorKeyArray[i]))
+f.close()
+
+# Creates a dictionary of original position keys
+# and the character type values and assigns the 
+# original positions to their chunk of type-integers
+d = {}
+for element in range(len(rKeyArray)):
+    d.update({rKeyArray[element]: typeArray[element]})
+
+
+# Re-constructs the character type array by retrieving 
+# values of the previously constructed dictionary
+actualKey = ""
+for i in range(len(d)):
+    actualKey += d.get(i)
+
+
+# Instantiation of a list to house the decrypted characters
+# and of the string that houses the decrypted message
+decodedLst = []
+decodedMesaage = ""
+
+
+# Simple method that accesses a list and returns the 
+# character at the provided index
+# *NOTE* the provided index is taken from the difference of
+# the array of indicies and the offset value
+def getChar(aLst, index):
+    decodedLst.append(aLst[index])
+
+
+ind = 0
+for i in range(len(actualKey)):
+    charIndex = int(IndexArray[ind]) - int(offset)
+    if actualKey[i] == '0' or actualKey[i] == '1':
+        #char in keyNum
+        getChar(keyNum, charIndex)
+        ind += 1
+    elif actualKey[i] == '2' or actualKey[i] == '3' or actualKey[i] == '4':
+        #char in keyLowerAlpha
+        getChar(keyLowerAlpha, charIndex)
+        ind += 1
+    elif actualKey[i] == '5' or actualKey[i] == '6' or actualKey[i] == '7':
+        #char in keyHigherAlpha
+        getChar(keyHigherAlpha, charIndex)
+        ind += 1
+    elif actualKey[i] == '8' or actualKey[i] == '9':
+        #char in keySPChar
+        getChar(keySPChar, charIndex)
+        ind += 1
+        
+
+decodedLst.reverse()    #Reverse the list to get the proper message
+
+of = open("Decoded Message.txt", "w")
+for eachElement in decodedLst:
+    of.write(eachElement)
+of.close()
---
+++ TextEncoder.py
@@ -0,0 +1,194 @@
+import random
+import sys
+# Methods that will return the new character based on 
+# the new index inputed
+def higherEncrypt(index):
+    return keyHigherAlpha[index]
+def lowerEncrypt(index):
+    return keyLowerAlpha[index]
+def numEncrypt(index):
+    return keyNum[index]   
+def spEncrypt(index):
+    return keySPChar[index]
+
+
+# Method that returns the index of the original character 
+# based on its location in the reference lists
+def getIndex(character):
+    if character in keyNum:
+        return keyNum.index(character)
+    elif character in keyLowerAlpha:
+        return keyLowerAlpha.index(character)
+    elif character in keyHigherAlpha:
+        return keyHigherAlpha.index(character)
+    elif character in keySPChar:
+        return keySPChar.index(character)
+
+
+# Method that checks the type of the original character
+# and returns a number based on its type
+def checkType(character):
+    if character in keyNum:
+        return random.choice(['0', '1'])
+    elif character in keyLowerAlpha:
+        return random.choice(['2', '3', '4'])
+    elif character in keyHigherAlpha:
+        return random.choice(['5', '6', '7'])
+    elif character in keySPChar:
+        return random.choice(['8', '9'])
+
+    
+# Collection of arrays that will serve as the lists that we
+# reference characters from 
+keyNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+keyLowerAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+keyHigherAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+keySPChar = ['!', '@', '#', '$', '%', '&', ' ', '_', '-', '+', '/','?','*', '.', ',' , ':', ';', "'", '!', '@', '#', '$', '%', '&', ' ', '_', '-', '+', '/','?','*', '.', ',' , ':', ';', "'"]
+
+             
+# Main 
+print("Enter what you like to encrypt: (1) a file containing a short message, (2) a short phrase that you provide, or (3) nothing (exit)")
+
+text = []
+while(True):
+    choice = int(input("Choice: "))
+    if choice == 1:
+        while(True):
+            try:
+                fileName = input("Enter the file name here: ")
+            except FileNotFoundError:
+                print("That file does not exist! Try again.")
+            else:
+                break
+        fileReader = open(fileName + ".txt", "r")
+
+        for eachLine in fileReader:
+            for element in eachLine:
+                if element != '\n':
+                    text.append(element)
+        fileReader.close()
+        break
+    
+    elif choice == 2:
+        message = input("Enter your message here: ")
+        for element in message:
+            if element != "\n":
+                text.append(element)
+        break
+        
+    elif choice == 3:
+        sys.exit()
+
+    else:
+        print("That's not a valid choice. Please try again.")
+        continue
+            
+print("What do you want to call the file where your message will be outputted?")
+outputName = input("Name: ")
+
+offset = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9])
+if offset > len(text):
+    offset = len(text)
+encryptionTypeKey = ""
+encryptionIndex = []
+encryptedText = ""
+encryption = ""
+
+for iter1 in range(len(text)):
+    #Will chose a random char type as the stand in for the current char
+    encryptionType = random.choice([0, 1, 2, 3])    
+    char = text.pop()
+    encryptionTypeKey += (checkType(char))
+    ind = getIndex(char)
+    newInd = ind + offset
+    encryptionIndex.append(newInd)
+    
+    # Switch that decides what type of character each element 
+    # will be presented as
+    # 0 will become numbers
+    # 1 will become lowercase letters
+    # 2 will become uppercase letters
+    # 3 will become special characters
+    if encryptionType == 0:
+        encryptedText += numEncrypt(newInd)
+    elif encryptionType == 1:
+        encryptedText += lowerEncrypt(newInd)
+    elif encryptionType == 2:
+        encryptedText += higherEncrypt(newInd)
+    elif encryptionType == 3:
+        encryptedText += spEncrypt(newInd)
+        
+
+# Following blocks are for the encryption key rotator
+# length chooses how many elements will be in each rotated element
+messageLength = len(encryptedText)
+if messageLength > 80:    
+    length = random.choice([8, 10, 12, 15])
+elif messageLength > 50:
+    length = random.choice([5, 7, 9])
+elif messageLength > 20:
+    length = random.choice([5, 6, 8])
+else:
+    length = random.choice([2, 3, 4])
+rotatorElements = []
+remainder = len(encryptionTypeKey) % length
+starting = 0
+
+# Breaks up the var encryptionTypeKey into chunks that can be changed around
+while(True):
+    rotator = ""
+    for i in range(length):
+        rotator += encryptionTypeKey[starting + i]
+    rotatorElements.append(rotator)
+    starting += length
+    if((len(encryptionTypeKey) - remainder) == starting):
+        break;
+        
+# In the common case that the sum of the lengths of each rotated chunk is 
+# not equal to the length of the encrypted phrase a remainder is formed to
+# add the last few indices to the rotator
+if remainder > 0:
+    rotator =""
+    for i in range(remainder):
+        rotator += encryptionTypeKey[starting + i]
+    rotatorElements.append(rotator)
+# rotationOrder will be the new altered encryptionTypeKey
+# rotatorKey will allow us to put the encryptionTypeKey back 
+# in the proper order
+rotationOrder = []
+rotatorKey = []
+numRotators = len(rotatorElements)
+
+# Creates the new rotatorKey and randomizes the order
+for i in range(numRotators):
+    rotatorKey.append(i)
+random.shuffle(rotatorKey)
+
+# Based on the order of the rotatorKey, the rotationOrder
+# will append the chunks of indices
+for element in rotatorKey:
+    rotationOrder.append(rotatorElements[element])
+    
+
+encryption = str(offset) + "^" + encryptedText 
+encryptionIndex.reverse()
+
+f = open(outputName + ".txt", "w")
+f.write("3NCRY\n")
+f.write(encryption + "\n")
+
+for element in rotationOrder:
+    string = str(element)
+    f.write(string + "^")
+f.write("\n")
+
+for element in encryptionIndex:
+    char = str(element)
+    f.write(char + '.')
+f.write("\n")
+
+for element in rotatorKey:
+    char = str(element)
+    f.write(char + '.')
+f.write("\n")
+f.close()
